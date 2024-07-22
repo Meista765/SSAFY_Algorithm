2e37e67910e2480e3425e1c0fc201759a5351321
Youngkwang Seo <joungnx123@naver.com>
2024-07-22 21:55:14 +0900

    구간 합 구하기 1 / 30분

--- 03_2/B_11659.py
+++ 03_2/B_11659.py
@@ -1,6 +1,9 @@
 import sys
 sys.stdin = open("C:/Users/82108/Downloads/input.txt", "r")
 
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())  # N: 수의 개수, M: 합을 구해야 하는 횟수
 num_list = list(map(int, input().split()))  # N개의 수
 
@@ -10,7 +13,6 @@ temp_sum = 0  # 구간 합
 for num in num_list:  # num_list 내 숫자 loop
     temp_sum += num  # temp_sum에 숫자를 하나씩 더하고
     sum_list.append(temp_sum)  # 각 구간 합을 sum_list에 저장
-
 
 for _ in range(M):  # M번 loop
     i, j = map(int, input().split())  # 질의 범위 받기
