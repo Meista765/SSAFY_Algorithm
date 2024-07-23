b50008c3c1e2aaf840c7533721a352b0442c044e
Macbook Air M1 <macbookairm1@Macbookui-MacBookAir.local>
2024-07-23 19:07:29 +0900

    나머지 합 구하기 / 796ms

--- B_10986.py
+++ B_10986.py
@@ -5,19 +5,33 @@ num_lst = list(map(int, input().split()))
 # 합 배열 구하기
 
 number = 0
-sum_lst = [0] # 합 배열
-c = [0] * M
+S = [0] * N # 합 배열
+C = [0] * M # 나머지 배열
 
+S[0] = num_lst[0]
+
+# 합 배열 만들기
 
 for i in range(1,N):
-    number += num_lst[i]
-    sum_lst.append(number)
-
-
+    S[i] = S[i-1] + num_lst[i]
 
 # 합 나머지 배열 구하기
 
+cnt = 0  # 합 나머지 배열이 0인 것 카운트
 for j in range(N):
+    remain_num = S[j] % M
+    if remain_num == 0:
+        cnt += 1
+    C[remain_num] += 1
+
+
+for k in range(M):
+    if C[k] > 1:
+        
+        cnt  += (C[k] * (C[k]-1)) //2
+
+
+print(cnt)
 
 
 
