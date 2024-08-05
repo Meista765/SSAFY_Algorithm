440ed4866cf98c097cfaeb5691291bc769a3b3cb
Macbook Air M1 <macbookairm1@Macbookui-MacBookAir.local>
2024-08-05 21:03:36 +0900

    8/04

--- SSAFY_실습문제/8.05/S_03143.py
+++ SSAFY_실습문제/8.05/S_03143.py
@@ -1,13 +1,23 @@
 T = int(input())
 
 for test_case in range(1,T+1):
-    T, P = input().split()
-    N = len(T) # 전체 텍스트
-    M = len(P) # 패턴의 길이
+    text, pattern = input().split()
+    
+    N = len(text) # 전체 텍스트
+    M = len(pattern) # 패턴의 길이
 
     cnt = 0
     i = 0 # t의 인덱스
-    j = 0 # p의 인덱스
-    while j < M and i < N:
-        if T[i] == P[j]:
-            i =
+    
+    while i+M-1 < N:
+        for j in range(M):
+            if pattern[j] != text[i+j]:
+                break
+        else: 
+            cnt += 1
+            i = i + M - 1
+        i += 1
+            
+    answer =  N-(M-1)*cnt
+    print(f'#{test_case} {answer}')
+
