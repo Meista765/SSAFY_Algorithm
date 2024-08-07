b28d8af8f0c81ed46011fa5203d5346e8c8be951
JoKiHeum <whrlgma98@gmail.com>
2024-08-07 13:33:47 +0900

    DFS-연결고리 찾기

--- 백준/B_11724.py
+++ 백준/B_11724.py
@@ -1,22 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-def DFS(arr,v,visited):
-    stack= [v]
-    visited[v] = True
-    
+def DFS(arr,s,visited):
+    stack = [s]
+    visited[s] = True
+    v = s
     
     while stack:                    # 스택이 빌때까지
-        for i in arr[stack.pop()]:
-            if not visited[i]:      # 방문한 적 없는 노드이면
-                stack.append(i)
-                visited[i] = True
-
+        for w in arr[v]:
+            if not visited[w]:      # 방문한 적 없는 노드이면
+                stack.append(v)
+                v = w
+                visited[w] = True
+                break
+        else:
+            v = stack.pop()
 
 node, edge = map(int,input().split()) # node의 수, edge의 수
 
 arr = [[] for _ in range(node+1)]     # 인접리스트
-visited = [False] * (node+1)              # 방문리스트
+visited = [False] * (node+1)          # 방문리스트
 for _ in range(edge):                 # 인접리스트 만들기
     u, v = map(int,input().split())
     arr[u].append(v)
@@ -25,11 +28,13 @@ for _ in range(edge):                 # 인접리스트 만들기
 
     
 cnt = 0
-for i in range(1,node+1):
-    if not visited[i]:      #만약 방문한 적 없는 노드면 DFS실행
-        DFS(arr,i,visited)
+for s in range(1,node+1):
+    if not visited[s]:      #만약 방문한 적 없는 노드면 DFS실행
+        DFS(arr,s,visited)
         cnt += 1
+
 print(cnt)
+
 
 
 
