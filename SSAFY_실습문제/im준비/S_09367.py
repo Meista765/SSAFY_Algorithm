2e44b0234f008896e3f6ed5d3be0a587b3097868
Macbook Air M1 <macbookairm1@Macbookui-MacBookAir.local>
2024-08-16 01:10:07 +0900

    당근

---
+++ SSAFY_실습문제/im준비/S_09367.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('input.txt')
+T = int(input())
+
+for test_case in range(1,T+1):
+    N = int(input())
+    arr = list(map(int,input().split()))
+    max_cnt = 1
+    cnt =1
+
+    for i in range(N-1):
+        if arr[i] < arr[i+1]:
+            cnt += 1
+        else:
+            if max_cnt < cnt:
+                max_cnt = cnt
+            cnt = 1         # cnt 초기화
+    if max_cnt < cnt:
+        max_cnt = cnt
+    print(f'#{test_case} {max_cnt}')
