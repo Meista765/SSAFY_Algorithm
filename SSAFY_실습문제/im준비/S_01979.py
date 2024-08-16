6547320f6d70285d47a71aa0c74e218c50441b7e
JoKiHeum <whrlgma98@gmail.com>
2024-08-16 09:36:56 +0900

    어디에 단어가 들어갈 수 있을까

--- SSAFY_실습문제/im준비/S_01979.py
+++ SSAFY_실습문제/im준비/S_01979.py
@@ -1,3 +1,5 @@
+import sys
+sys.stdin = open('input.txt')
 T = int(input())
 
 for test_case in range(1,T+1):
@@ -8,13 +10,35 @@ for test_case in range(1,T+1):
     # 가로 탐색
     for i in range(N):
         cnt = 0
-        for j in range(N-K+1):
+        for j in range(N):
             if arr[i][j] == 1:
                 cnt += 1
-            elif arr[i][j] == 0 and j:
+            else:
                 if cnt == K:
                     answer += 1
+
                 cnt = 0
+        # 마지막이 1로 끝났을 경우를 위해 한번 더 비교
+        if cnt == K:
+            answer += 1
+
+
+    # 열 탐색
+    for j in range(N):
+        cnt = 0
+        for i in range(N):
+            if arr[i][j] == 1:
+                cnt += 1
+            else:
+                if cnt == K:
+                    answer +=1
+                cnt = 0
+        # 마지막이 1로 끝났을 경우를 위해 한번 더 비교
+        if cnt == K:
+            answer += 1
+
+
+    print(f'#{test_case} {answer}')
 
 
 
