32506ac9918bc4c70d988f84897235d5bd4e4071
ngoolglory <joungnx123@naver.com>
2024-08-26 17:49:03 +0900

    선발명단

---
+++ Back_tracking/B_03980.py
@@ -0,0 +1,6 @@
+# 선발 명단
+import sys
+sys.stdin = open('C:/Users/SSAFY/Downloads/sample_input.txt', 'r')
+#sys.stdin = open("C:/Users/82108/Downloads/sample_input.txt", "r")
+#input = sys.stdin.readline
+
