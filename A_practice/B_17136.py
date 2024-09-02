1c78781598b6bee9ef07575a9686e346a2bfbc14
Youngkwang Seo <joungnx123@naver.com>
2024-09-02 21:45:57 +0900

    색종이 붙이기

---
+++ A_practice/B_17136.py
@@ -0,0 +1,5 @@
+# 색종이 붙이기
+import sys
+#sys.stdin = open('C:/Users/SSAFY/Downloads/sample_input.txt', 'r')
+sys.stdin = open("C:/Users/82108/Downloads/sample_input.txt", "r")
+#input = sys.stdin.readline
