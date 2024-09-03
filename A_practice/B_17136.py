bae3f7adaea2950e473c0fe6fb8abaf01a365a46
ngoolglory <ngoolseo@gmail.com>
2024-09-03 16:22:04 +0900

    삭제

--- A_practice/B_17136.py
+++
@@ -1,5 +0,0 @@
-# 색종이 붙이기
-import sys
-#sys.stdin = open('C:/Users/SSAFY/Downloads/sample_input.txt', 'r')
-sys.stdin = open("C:/Users/82108/Downloads/sample_input.txt", "r")
-#input = sys.stdin.readline
