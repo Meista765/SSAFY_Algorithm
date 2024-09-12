00eb4b1212303d9e5af5712b89ebe8faa5704bf2
Macbook Air M1 <macbookairm1@Macbookui-MacBookAir.local>
2024-09-13 01:47:11 +0900

    하나로-prim

--- SSAFY_실습문제/9.11/S_01251.py
+++ SSAFY_실습문제/9.11/S_01251.py
@@ -6,26 +6,53 @@
 
 import sys
 sys.stdin = open('input.txt')
+from heapq import heappop,heappush
 
 def prim(start):
-    dist[start] = 0
+    D[start] = 0
+    hq = [(D[start],start)]
+    cnt = N
+    pay = 0
 
+    while cnt:
+        weight,v = heappop(hq)
 
+        # 방문한 노드인지 체크
+        if visited[v]:
+            continue
 
+        # 방문하지 않은 노드라면
+        visited[v] = 1      # 방문체크
+        pay += E * weight
 
+        # 노드 확인
+        for w,weight in G[v]:
+            if not visited[w] and D[w] > weight:
+                D[w] = weight
+                p[w] = v
+                heappush(hq,(D[w],w))
+        cnt -= 1
+    return pay
 
 
 T = int(input())
 for test_case in range(1,T+1):
-    N = int(input())
+    N = int(input())                    # 섬의 갯수
     X = list(map(int,input().split()))
     Y = list(map(int,input().split()))
     E = float(input())
 
-    G = [[] for _ in range(N)]
-    visited = [0] * N
-    D = [(float('inf'))] * N
+    G = [[] for _ in range(N)]     # 인접리스트
+    visited = [0] * N               # 방문 정보 리스트
+    D = [(float('inf'))] * N      # 간선 정보
+    p = [i for i in range(N)]
 
+    # 인접리스트 생성
     for i in range(N-1):
         for j in range(i+1, N):
-            dist = X[i] - X[]
+            distance = (X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2
+            G[i].append((j, distance))
+            G[j].append((i, distance))
+
+    result = round(prim(0))
+    print(f'#{test_case} {result}')
