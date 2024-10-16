0db4c8925c5a0bcc7c6ffd4cc924c19e6799570d
Hyunbeen <hyunbin322@gmail.com>
2024-10-16 17:43:38 +0900

    10_16_강의실

--- SWEA/SWEA_11611_배열최소합_확인용/SWEA_11611.py
+++ SWEA/SWEA_11611_배열최소합_확인용/SWEA_11611.py
@@ -1,25 +1,22 @@
 import sys
-sys.stdin = open('C:/Users/SSAFY/Desktop/깃/SSAFY_Algorithm/SWEA/SWEA_11611_배열최소합_확인용/sample_input.txt', 'r')
+sys.stdin = open('sample_input.txt', 'r')
 
 def permutation(k, N):
-    # 외부 변수 호출
     global min_sum
     sum_value = 0
     if k == N:
         for i in range(N):
             sum_value +=  matrix[i][arr[i]]
-            # 더하는 중에 min_sum 넘어가면 다음 반복
-            ## 교수님 코드 참고
             if sum_value > min_sum:
-                break
+                return
         if sum_value < min_sum:
             min_sum = sum_value
+            return
     else:
         for i in range(k, N):
             arr[k], arr[i] = arr[i], arr[k]
             permutation(k + 1, N)
-            arr[k], arr[i] = arr[i], arr[k]
-    return min_sum
+            arr[k], arr[i] = arr[i], arr[k] 
 
 T = int(input())
 
@@ -27,8 +24,11 @@ for tc in range(1, T + 1):
     N = int(input())
     matrix = [list(map(int, input().split())) for _ in range(N)]
     arr = list(range(N))
-    min_sum = float('inf')
+    min_sum = 10 * N
     
-    minimum = permutation(0, N)
+    permutation(0, N)
     
-    print(f'#{tc} {minimum}')
+    print(f'#{tc} {min_sum}')
+
+
+
