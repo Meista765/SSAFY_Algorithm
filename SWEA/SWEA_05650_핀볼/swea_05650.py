94884f8f856c5f6b624aa4bf5c55aa44c16881ca
HyunbeenLim <hyunbin322@gmail.com>
2024-11-05 20:03:34 +0900

    11_5_데일리

--- SWEA/SWEA_05650_핀볼/swea_05650.py
+++ SWEA/SWEA_05650_핀볼/swea_05650.py
@@ -5,32 +5,14 @@ sys.stdin = open('sample_input.txt', 'r')
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
+# 블럭에 부딪힌 뒤 바뀌는 방향
 blocks = [
-    [],     # 0번
-    {
-        0:2,
-        1:0,
-        2:3,
-        3:1
-    },     # 1번
-    {
-        0:2,
-        1:3,
-        2:1,
-        3:0
-    },     # 2번
-    {
-        0:1,
-        1:3,
-        2:0,
-        3:2
-    },     # 3번
-    {
-        0:3,
-        1:2,
-        2:0,
-        3:1
-    }     # 4번
+    [],
+    [2, 0, 3, 1], 
+    [2, 3, 1, 0],
+    [1, 3, 0, 2],
+    [3, 2, 0, 1],
+    [2, 3, 0, 1]
 ]
 
 # 웜홀 짝 찾기
@@ -42,64 +24,49 @@ def find_wormhole(matrix):
             if matrix[r][c] in [6, 7, 8, 9, 10]:
                 wormhole[matrix[r][c] - 6].append((r, c))
     return wormhole
-
-# 점수 계산 함수
+        
+# 코드 깔끔하게
 def game_play(start_r, start_c, wormhole, start_direction):
     global max_score
-
-    score  = 0
+    
+    score = 0
     current_r = start_r
     current_c = start_c
     current_direction = start_direction
-
+    
     while True:
         nr = current_r + dr[current_direction]
         nc = current_c + dc[current_direction]
-
-        if (0 <= nr < N) and (0 <= nc < N):
+        
+        # 인덱스를 벗어나는 순간 벽에 부딪힌 것으로 판정, 점수 올리고 방향만 바꿔주기
+        if not (0 <= nr < N) or not (0 <= nc < N):
+            score += 1
+            current_direction = (current_direction + 2) % 4
+            
+            # 벽에 부딪히면 field 탐색 X => 인덱스를 벗어난 상태의 좌표를 현재 위치로 지정해줘도 괜찮음
+            # 방향이 바뀌기 때문에 다음 nr, nc는 무조건 field 안의 좌표
+            current_r, current_c = nr, nc
+        # nr, nc가 field 안의 좌표일 때
+        else:
             value = field[nr][nc]
             current_r, current_c = nr, nc
-
             # 게임 종료 규칙
-            if (value == -1) or ((current_r, current_c) == (start_r, start_c)):
+            if value == -1 or (current_r, current_c) == (start_r, start_c):
                 break
-            # 사각 블럭
-            elif value == 5:
+            # 블럭 만났을 때
+            elif value in [1, 2, 3, 4, 5]:
+                # 점수 올리기
                 score += 1
-                current_direction = (current_direction + 2) % 4
-            # 다른 블럭
-            elif value in [1, 2, 3, 4]:
-                score += 1
+                # 방향 바꾸기
                 current_direction = blocks[value][current_direction]
-                # 방향이 바뀐 직후 다음 갈 곳이 인덱스를 넘어갈 때
-                temp_r = current_r + dr[current_direction]
-                temp_c = current_c + dc[current_direction]
-                if not (0 <= temp_r < N) or not (0 <= temp_c < N):
-                    score += 2
-                    # 벽에 부딪힌 후
-                    current_direction = (current_direction + 2) % 4
-                    # 다시 블럭 만나기
-                    current_direction = blocks[value][current_direction]
             # 웜홀
             elif value in [6, 7, 8, 9, 10]:
                 pairs =  wormhole[value - 6]
+                # 웜홀은 번호당 한쌍만 존재, 현재 좌표와 다른 좌표가 이동할 웜홀의 좌표
                 for position in pairs:
                     if (current_r, current_c) != position:
                         current_r, current_c = position
                         break
-            elif value == 0:
-                # 벽을 만났을 때
-                if (current_direction in [1, 3]) and (current_r in [0, N-1]):
-                    score += 1
-                    current_direction = (current_direction + 2) % 4
-                elif (current_direction in [0, 2]) and (current_c in [0, N-1]):
-                    score += 1
-                    current_direction = (current_direction + 2) % 4
-                continue
-        # 시작부터 인덱스 넘어가는 경우
-        else:
-            break
-
     if max_score < score:
         max_score = score
 
