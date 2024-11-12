e3f3e5ca7586cd011734e85efa1e6acb9180b4a2
HyunbeenLim <hyunbin322@gmail.com>
2024-11-12 19:52:13 +0900

    11_12_데일리

--- temp.py
+++ temp.py
@@ -1,5 +1,18 @@
-w = 3
+from itertools import combinations, permutations, product
 
-e = (w + 1) % 4
 
-print(e)
+N = 4
+
+combination = list(combinations(list(range(N)), 2))
+permutation = list(permutations(list(range(N)), 2))
+prod = list(product(list(range(N)), repeat=2))
+
+print(f'combinations {combination}')
+print(f'permutations {permutation}')
+print(f'prod {prod}')
+
+
+combi = combinations(list(range(N)), 2)
+
+for comb in combi:
+    print(comb)
